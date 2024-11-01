b2d943953287e8fa08d1994d7450bc3e6ca85495
tammytamayao <tammytamayaogmail.com@Princesss-MacBook-Pro.local>
2024-11-01 19:31:11 +0800

    fix: quotations marks and whitespaces

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -17,11 +17,10 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-      origins 'http://localhost:3001'
-      resource '*',
+      origins "http://localhost:3001"
+      resource "*",
         headers: :any,
-        methods: [:get, :post, :put, :patch, :delete, :options, :head],
+        methods: [ :get, :post, :put, :patch, :delete, :options, :head ],
         credentials: false # for cookies or authentication
     end
   end
-  
