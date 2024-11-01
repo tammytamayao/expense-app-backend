8a02a77e70a1a7a905fd87007f074d06f88dcdbf
tammytamayao <tammytamayaogmail.com@Princesss-MacBook-Pro.local>
2024-11-01 14:02:34 +0800

    feat: CRUD expense API

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -1,2 +1,5 @@
 class Expense < ApplicationRecord
+    validates :title, presence: true
+    validates :description, presence: true
+    validates :amount, presence: true, numericality: { greater_than_or_equal_to: 0 }
 end
