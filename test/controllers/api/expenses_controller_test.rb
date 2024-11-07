1e425292885ed612072de3fe66a718c885568450
tammytamayao <tammytamayaogmail.com@Princesss-MacBook-Pro.local>
2024-11-07 09:38:10 +0800

    fix: change single to double quotation marks

--- test/controllers/api/expenses_controller_test.rb
+++ test/controllers/api/expenses_controller_test.rb
@@ -1,4 +1,4 @@
-require 'test_helper'
+require "test_helper"
 
 class Api::ExpensesControllerTest < ActionDispatch::IntegrationTest
   # Setup users for testing
@@ -8,43 +8,43 @@ class Api::ExpensesControllerTest < ActionDispatch::IntegrationTest
     @expense_one = expenses(:one)
   end
 
-  # Test 'index' action
+  # Test "index" action
   test "should get expenses" do
     get api_expenses_url, headers: { Username: @user_one.username }
     assert_response :success
     json = JSON.parse(response.body)
-    assert_equal @user_one.id, json['expenses'][0]['user_id']
+    assert_equal @user_one.id, json["expenses"][0]["user_id"]
   end
 
-  # Test 'create' action
+  # Test "create" action
   test "should create expense" do
-    expense_params = { expense: { title: 'New Title', description: 'New Description', amount: 10.25, date: '2024-11-07' } }
+    expense_params = { expense: { title: "New Title", description: "New Description", amount: 10.25, date: "2024-11-07" } }
     post api_expenses_url, params: expense_params, headers: { Username: @user_one.username }
     assert_response :created
     json = JSON.parse(response.body)
-    assert_equal 'New Title', json['title']
-    assert_equal @user_one.id, json['user_id']
+    assert_equal "New Title", json["title"]
+    assert_equal @user_one.id, json["user_id"]
   end
 
-  # Test 'show' action
+  # Test "show" action
   test "should show expense" do
     get api_expense_url(@expense_one), headers: { Username: @user_one.username }
     assert_response :success
     json = JSON.parse(response.body)
-    assert_equal @expense_one.title, json['title']
+    assert_equal @expense_one.title, json["title"]
   end
 
-  # Test 'update' action
+  # Test "update" action
   test "should update expense" do
-    patch api_expense_url(@expense_one), params: { expense: { title: 'Updated Title' } }, headers: { Username: @user_one.username }
+    patch api_expense_url(@expense_one), params: { expense: { title: "Updated Title" } }, headers: { Username: @user_one.username }
     assert_response :success
     @expense_one.reload
-    assert_equal 'Updated Title', @expense_one.title
+    assert_equal "Updated Title", @expense_one.title
   end
 
-  # Test 'destroy' action
+  # Test "destroy" action
   test "should destroy expense" do
-    assert_difference('Expense.count', -1) do
+    assert_difference("Expense.count", -1) do
       delete api_expense_url(@expense_one), headers: { Username: @user_one.username }
     end
     assert_response :no_content
