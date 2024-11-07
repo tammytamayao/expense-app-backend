719480b3f5442cf1765450b467e6a193be2bf7f9
tammytamayao <tammytamayaogmail.com@Princesss-MacBook-Pro.local>
2024-11-07 11:03:05 +0800

    test: add model test code

--- test/models/expense_test.rb
+++ test/models/expense_test.rb
@@ -1,7 +1,43 @@
-require "test_helper"
+require 'test_helper'
 
 class ExpenseTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should not save expense without title" do
+    expense = Expense.new(description: "Test description", amount: 100, date: Date.today, user_id: users(:one).id)
+    assert_not expense.save
+  end
+
+  test "should not save expense without description" do
+    expense = Expense.new(title: "Test title", amount: 100, date: Date.today, user_id: users(:one).id)
+    assert_not expense.save
+  end
+
+  test "should not save expense without amount" do
+    expense = Expense.new(title: "Test title", description: "Test description", date: Date.today, user_id: users(:one).id)
+    assert_not expense.save
+  end
+
+  test "should not save expense with negative amount" do
+    expense = Expense.new(title: "Test title", description: "Test description", amount: -1, date: Date.today, user_id: users(:one).id)
+    assert_not expense.save
+  end
+
+  test "should not save expense without date" do
+    expense = Expense.new(title: "Test title", description: "Test description", amount: 100, user_id: users(:one).id)
+    assert_not expense.save
+  end
+
+  test "should not save expense with future date" do
+    expense = Expense.new(title: "Test title", description: "Test description", amount: 100, date: Date.tomorrow, user_id: users(:one).id)
+    assert_not expense.save
+  end
+
+  test "should save valid expense" do
+    expense = Expense.new(title: "Test title", description: "Test description", amount: 100, date: Date.today, user_id: users(:one).id)
+    assert expense.save
+  end
+
+  test "should belong to a user" do
+    expense = Expense.new(title: "Test title", description: "Test description", amount: 100, date: Date.today, user_id: users(:one).id)
+    assert_equal expense.user.username, users(:one).username
+  end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,35 @@
-require "test_helper"
+require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should not save user without username" do
+    user = User.new(password: "password")
+    assert_not user.save
+  end
+
+  test "should not save user with non-unique username" do
+    user1 = users(:one)
+    user2 = User.new(username: user1.username, password: "newpassword")
+    assert_not user2.save
+  end
+
+  test "should not save user without password" do
+    user = User.new(username: "newuser")
+    assert_not user.save
+  end
+
+  test "password should be encrypted" do
+    user = User.new(username: "encrypteduser", password: "password")
+    user.save
+    assert_not_equal "password", user.password_digest
+  end
+
+  test "should save user with valid username and password" do
+    user = User.new(username: "validuser", password: "password")
+    assert user.save
+  end
+
+  test "should have many expenses" do
+    user = users(:one)
+    assert_equal user.expenses.count, 1
+  end
 end
